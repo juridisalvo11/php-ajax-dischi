88c67a204e750effee31ee6af895b80b551da7d5
Juri <juridisalvo11@gmail.com>
2020-06-17 20:02:46 +0200

    php - dischi

---
+++ ajax/index.php
@@ -0,0 +1,13 @@
+<?php echo 'ciao' ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="public/app.css">
+  </head>
+  <body>
+    <script src="public/app.js" charset="utf-8"></script>
+  </body>
+</html>
---
+++ php/index2.php
@@ -0,0 +1,41 @@
+<?php
+
+require ('dischi.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="dist/app.css">
+  </head>
+  <body>
+    <header>
+      <div class="container">
+        <div class="box-logo">
+          <img src="img/logo.png" alt="">
+        </div>
+      </div>
+    </header>
+    <main>
+      <div class="container">
+        <section class="cd-box">
+          <?php
+          foreach ($dischi as $disco) { ?>
+            <div class="album-box">
+            <img src=" <?php echo $disco['poster']?>" alt="">
+            <h3><?php echo $disco['title'] ?></h3>
+            <p><?php echo $disco['author'] ?></p>
+            <p><?php echo $disco['genre'] ?></p>
+            <p><?php echo $disco['year'] ?></p>
+            </div>
+            <?php
+          }
+          ?>
+        </section>
+      </div>
+    </main>
+  </body>
+</html>
